3a9f2a4a87ac87e88eeae89d4e3c11f5cf1c39d9
Nick0603 <b10303008@gmail.com>
2017-04-01 14:51:43 +0800

    整合bluetoothChat 程式碼  將其conversation改成單一textview呈現

--- app/src/main/java/acase/cprcase/SettingActivity.java
+++ app/src/main/java/acase/cprcase/SettingActivity.java
@@ -1,20 +1,33 @@
 package acase.cprcase;
 
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import acase.cprcase.bluetooth.BluetoothChatFragment;
+
 
 public class SettingActivity extends AppCompatActivity {
 
+    private static final String TAG = "Setting";
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bluebooth);
+        setContentView(R.layout.activity_setting);
         setToolBar();
+
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            BluetoothChatFragment fragment = new BluetoothChatFragment();
+            transaction.replace(R.id.sample_content_fragment, fragment);
+            transaction.commit();
+        }
     }
+
+
 
     void setToolBar(){
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -23,4 +36,7 @@ public class SettingActivity extends AppCompatActivity {
         actionBar.setTitle("Setting");
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
+
+
+
 }
